3c916f1e9c4b0da6841c357236487805c994ceb6
takatsukayuriko <takayuri0906@gmail.com>
2020-08-20 17:08:57 +0900

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const url = 'https://api.a3rt.recruit-tech.co.jp/text_summarization/v1';
-const apikey = 'DZZftGjQWTvlOiUcrmHoyvwENpdYIb3n';
+const apikey = 'XXXXXXXXXXXXXXXXXXX';
 
-var sentence = document.getElementById("sentence").value;
 var button = document.getElementById("btn");
 
 button.addEventListener("click", event => {
-    var sentence = document.getElementById("sentence").value;
+    var sentence = document.getElementById("sentence").value; //入力文章を取得
     const params = new URLSearchParams();
     params.append("apikey",apikey);
     params.append("sentences",sentence);
@@ -14,6 +13,7 @@ button.addEventListener("click", event => {
     
     axios.post(url,params)
         .then(result =>
+            //要約結果を代入、HTMLで表示
             {document.getElementById("summarized").innerHTML = JSON.stringify(result.data.summary[0],null,2);})
         .catch(error => {console.log(error);
     });
