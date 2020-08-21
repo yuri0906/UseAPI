35b756fec4b1ffc9413c11ae3df7f3d6b785b292
takatsukayuriko <takayuri0906@gmail.com>
2020-08-22 01:39:42 +0900

    use vue

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const url = 'https://api.a3rt.recruit-tech.co.jp/text_summarization/v1';
-const apikey = 'XXXXXXXXXXXXXXXXXXX';
-
-var button = document.getElementById("btn");
-
-button.addEventListener("click", event => {
-    var sentence = document.getElementById("sentence").value; //入力文章を取得
-    const params = new URLSearchParams();
-    params.append("apikey",apikey);
-    params.append("sentences",sentence);
-    params.append("linenumber",1);
-    params.append("separation","。");
-    
-    axios.post(url,params)
-        .then(result =>
-            //要約結果を代入、HTMLで表示
-            {document.getElementById("summarized").innerHTML = JSON.stringify(result.data.summary[0],null,2);})
-        .catch(error => {console.log(error);
-    });
-
+new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            summary: ""
+        };
+    },
+    methods: {
+        showMessage: function () {
+            const url = 'https://api.a3rt.recruit-tech.co.jp/text_summarization/v1';
+            const apikey = 'DZZftGjQWTvlOiUcrmHoyvwENpdYIb3n';
+            var params = new URLSearchParams();
+            params.append("apikey",apikey);
+            params.append("sentences", document.querySelector('textarea').value);
+            axios.post(url,params)
+                .then(response =>{
+                    this.summary = JSON.stringify(response.data.summary[0],null,2);
+            }).catch(error => {
+                    return error;
+            });
+        }
+    }
 });
-
