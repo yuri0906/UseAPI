5e60368c5f378e19626c7f2ccec99016d84b430c
takatsukayuriko <takayuri0906@gmail.com>
2020-08-26 21:16:30 +0900

    言語選択追加

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const url = 'https://api.a3rt.recruit-tech.co.jp/text_summarization/v1';
-const apikey = 'XXXXXXXXXXXXXXXXXXX';
+const apikey = 'DZZftGjQWTvlOiUcrmHoyvwENpdYIb3n';
 
 var button = document.getElementById("btn");
 
 button.addEventListener("click", event => {
+    var lang = document.getElementById("lang").value; //言語設定を取得
+    var separation = "";　//句読点
     var sentence = document.getElementById("sentence").value; //入力文章を取得
+    //言語によって句読点分岐
+    switch (lang){
+        case "ja":
+          separation = "。";
+          break;
+        case "en":
+            separation = ".";
+          break;
+        default:
+            console.log('言語を選択してください');
+      }
+
     const params = new URLSearchParams();
     params.append("apikey",apikey);
     params.append("sentences",sentence);
     params.append("linenumber",1);
-    params.append("separation","。");
+    params.append("separation",separation);
     
     axios.post(url,params)
         .then(result =>
