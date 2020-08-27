db324a0e7051b4ed06488181290e38c2fb541e5a
takatsukayuriko <takayuri0906@gmail.com>
2020-08-27 16:22:34 +0900

    CSS追加

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 var http = require('http');
 var html = require('fs').readFileSync('./index.html');
+const url = 'https://api.a3rt.recruit-tech.co.jp/text_summarization/v1';
+const apikey = 'XXXXXXXXXXXX';
 
 var server = http.createServer(function(request, response) { 
   response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   
     if(request.method === 'POST') {
-  
         //ここに処理を記述する
-  
     }
   response.end(html);
 })
 
 server.listen(8080);
 
-
-module.exports = function(sentences){
-  const url = 'https://api.a3rt.recruit-tech.co.jp/text_summarization/v1';
-  const apikey = 'DZZftGjQWTvlOiUcrmHoyvwENpdYIb3n';
+//以下単体でAPIを叩く関数
+/*
+function showSummary(sentences){
   var request = require('request');
   var options = {
     uri: url,
@@ -35,3 +34,4 @@ module.exports = function(sentences){
      return body.summary[0]; 
   }); 
 };
+*/
